9e09a16879ee8455f6996947ed9c595b37a4d0b2
Luis Rosal <luiss.rosalg.13@gmail.com>
2023-11-10 03:02:12 +0000

    first commit

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Characters, Planets, Favorites
 #from models import Person
 
 app = Flask(__name__)
@@ -30,14 +30,194 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
-@app.route('/user', methods=['GET'])
-def handle_hello():
+@app.route('/user', methods=['GET', 'POST'])
+def handle_user():
+    if request.method == 'GET':
+        get_user = User.query.all()
+
+        if get_user is None:
+            return jsonify({
+                "msg": "There are no users yet!"
+            }), 400
+
+        user_list = list(map(lambda user: user.serialize(), get_user))
+
+        return jsonify(user_list), 200
+
+    body = request.json    
+    new_user = User.create(body)   
+
+    if type(new_user) == dict:   
+        return jsonify({
+            "msg": new_user["msg"]
+        }), new_user["status"]
+
+    response_body = {     
+        "user": new_user.serialize()
+    }
+    return jsonify(response_body), 200
+    
+@app.route('/user/favorites', methods=['GET']) #da para solo un user al no ser dinámico
+def get_user_favs():
+    get_favs = Favorites.query.filter_by(user_id = "1")
+
+    if get_favs is None:
+        return jsonify({
+            "msg": "There are no favorites yet!"
+        }), 400
+
+    favs_list = list(map(lambda fav: fav.serialize(), get_favs))
+
+    return jsonify(favs_list), 200
+
+@app.route('/people', methods=['GET', 'POST'])
+def handle_people():
+    if request.method == 'GET':
+        get_characters = Characters.query.all()
+
+        if get_characters is None:
+            return jsonify({
+                "msg": "There are no characters yet!"
+            }), 400
+
+        characters_list = list(map(lambda char: char.serialize(), get_characters))
+
+        return jsonify(characters_list), 200
+    
+    body = request.json   
+    new_char = Characters.create(body)
+    if type(new_char) == dict:
+        return jsonify({
+            "msg": new_char["msg"]
+        }), new_char["status"]
 
     response_body = {
-        "msg": "Hello, this is your GET /user response "
+        "character": new_char.serialize()
     }
-
     return jsonify(response_body), 200
+    
+@app.route('/planets', methods=['GET', 'POST'])
+def handle_planets():
+    if request.method == 'GET':
+        get_planets = Planets.query.all()
+
+        if get_planets is None:
+            return jsonify({
+                "msg": "There are no planets yet!"
+            }), 400
+
+        planets_list = list(map(lambda planet: planet.serialize(), get_planets))
+
+        return jsonify(planets_list), 200
+    body = request.json 
+    new_planet = Planets.create(body) 
+    if type(new_planet) == dict:
+        return jsonify({
+            "msg": new_planet["msg"]
+        }), new_planet["status"]
+
+    response_body = {
+        "planet": new_planet.serialize()
+    }
+    return jsonify(response_body), 200
+
+@app.route('/people/<int:people_id>', methods=['GET'])
+def get_person(people_id):
+    get_person = Characters.query.get_or_404(people_id)
+
+    response_body = {
+        "character": get_person.character_name,
+        "birth_year": get_person.birth_year,
+        "gender": get_person.gender,
+        "skin_color": get_person.skin_color,
+        "height": get_person.height
+    }
+    return jsonify(response_body), 200
+
+@app.route('/planets/<int:planet_id>', methods=['GET'])
+def get_planet(planet_id):
+    get_planet = Planets.query.get_or_404(planet_id)
+
+    response_body = {
+        "planet_name": get_planet.planet_name,
+        "climate": get_planet.climate,
+        "terrain": get_planet.terrain,
+        "population": get_planet.population
+    }
+    return jsonify(response_body), 200
+
+@app.route('/favorite/people/<int:people_id>', methods=['POST', 'DELETE'])
+def handle_fav_person(people_id):
+    body = request.json
+    if request.method == 'POST':
+        new_fav_char = Favorites.fav_char(Favorites, body, people_id)
+
+        if type(new_fav_char) == dict:
+            return jsonify({
+                "msg": new_fav_char["msg"]
+            }), new_fav_char["status"]
+
+        response_body = {
+            "favorite": new_fav_char.serialize_char()
+        }
+
+        return jsonify(response_body), 200
+
+    get_characters = Favorites.query.filter_by(user_id = body["user_id"],character_id = people_id).first() 
+
+    if get_characters is None:
+        return jsonify({
+            "msg": "This character is not a favorite"
+        }), 400
+
+    delete_instance = Favorites.delete_and_commit(get_characters)
+
+    if delete_instance is False:
+        return jsonify ({
+            "msg": "Could not delete your favs"
+        }), 400
+
+    response_body = {
+        "msg": "Favorites deleted successfully"
+    }
+    return jsonify(response_body), 200
+
+@app.route('/favorite/planet/<int:planet_id>', methods=['POST', 'DELETE'])
+def handle_fav_planet(planet_id):
+    body = request.json
+    if request.method == 'POST':
+        new_fav_planet = Favorites.fav_planet(Favorites, body, planet_id)
+
+        if type(new_fav_planet) == dict:
+            return jsonify({
+                "msg": new_fav_planet["msg"]
+            }), new_fav_planet["status"]
+
+        response_body = {
+            "favorite": new_fav_planet.serialize_planet()
+        }
+
+        return jsonify(response_body), 200
+
+    get_planets = Favorites.query.filter_by(user_id = body["user_id"], planet_id = planet_id).first()
+
+    if get_planets is None:
+        return jsonify({
+            "msg": "This planet is not a favorite"
+        }), 400
+    
+    delete_instance = Favorites.delete_and_commit(get_planets)
+
+    if delete_instance is False:
+        return jsonify ({
+            "msg": "Could not delete your favs"
+        }), 400
+
+    response_body = {
+        "msg": "Favorites deleted successfully"
+    }
+    return jsonify(response_body), 200
+
 
 # this only runs if `$ python src/main.py` is executed
 if __name__ == '__main__':
--- src/models.py
+++ src/models.py
@@ -4,16 +4,352 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(70), nullable=False, unique=True)
+    first_name = db.Column(db.String(70), nullable=False)
+    last_name = db.Column(db.String(70), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    password = db.Column(db.String(80), nullable=False)
+    favorites = db.relationship("Favorites", backref="favorites_user")
+
+    @classmethod
+    def create(cls, body):
+        try:
+            if body.get("first_name") is None:
+                raise Exception ({
+                    "msg": "Your first name can't be empty",
+                    "status": 400
+                })
+            if body.get("last_name") is None:
+                raise Exception ({
+                    "msg": "Your last name can't be empty",
+                    "status": 400
+                })
+            if body.get("password") is None:
+                raise Exception ({
+                    "msg": "Your password can't be empty",
+                    "status": 400
+                })
+            if body.get("email") is None:
+                raise Exception ({
+                    "msg": "Your email can't be empty",
+                    "status": 400
+                })
+
+            user_exist = cls.query.filter_by(email = body["email"]).one_or_none()
+
+            if user_exist:
+                raise Exception ({
+                    "msg": "This email is already registered. Try another one",
+                    "status": 400
+                })
+            
+            new_user = cls(username = body["username"], first_name = body["first_name"], last_name = body["last_name"], email = body["email"], password = body["password"])
+
+            if not isinstance(new_user, cls):
+                raise Exception ({
+                    "msg": "Server Error",
+                    "status": 500
+                })
+            
+            save_instance = new_user.save_and_commit()
+
+            if save_instance is False:
+                raise Exception ({
+                    "msg": "Server Error",
+                    "status": 500
+                })
+
+            return new_user
+            
+        except Exception as error:
+            return ({
+                "msg": "Something went wrong (" + error.args[0]["msg"] + ")",
+                "status": error.args[0]["status"]
+            })
 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'<User {self.email}>'
 
     def serialize(self):
         return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            "name": self.first_name + " " + self.last_name,
+            "username": self.username,
+            "email": self.email
         }
+
+    def save_and_commit(self):
+        try:
+            db.session.add(self) 
+            db.session.commit()
+            return True
+        except Exception as error:
+            db.session.rollback()
+            return False
+
+
+class Characters(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    character_name = db.Column(db.String(70), unique=True, nullable=False)
+    skin_color = db.Column(db.String(70))
+    gender = db.Column(db.String(70))
+    birth_year = db.Column(db.String(70))
+    height = db.Column(db.String(70))
+    favorites = db.relationship("Favorites", backref="favorites_char")
+
+    @classmethod
+    def create(cls, body):
+        try:
+            if body.get("character_name") is None:
+                raise Exception ({
+                    "msg": "Your must write the character's name",
+                    "status": 400
+                })
+
+            character_exist = cls.query.filter_by(character_name = body["character_name"]).one_or_none()
+
+            if character_exist:
+                raise Exception ({
+                    "msg": "This character is already registered. Try another one or adding more info to the name",
+                    "status": 400
+                })
+            
+            new_character = cls(character_name = body["character_name"], birth_year = body.get("birth_year"), gender = body.get("gender"), skin_color = body.get("skin_color"), height = body.get("height"))
+
+            if not isinstance(new_character, cls):
+                raise Exception ({
+                    "msg": "Server Error",
+                    "status": 500
+                })
+            
+            save_instance = new_character.save_and_commit()
+
+            if save_instance is False:
+                raise Exception ({
+                    "msg": "Server Error",
+                    "status": 500
+                })
+
+            return new_character
+            
+        except Exception as error:
+            return ({
+                "msg": "Something went wrong (" + error.args[0]["msg"] + ")",
+                "status": error.args[0]["status"]
+            })
+
+    def __repr__(self):
+        return f'<Character {self.character_name}>'
+
+    def serialize(self):
+        return {
+            "name": self.character_name
+        }
+
+    def save_and_commit(self):
+        try:
+            db.session.add(self) 
+            db.session.commit()
+            return True
+        except Exception as error:
+            db.session.rollback()
+            return False
+
+
+class Planets(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    planet_name = db.Column(db.String(70), unique=True, nullable=False)
+    climate = db.Column(db.String(70))
+    terrain = db.Column(db.String(70))
+    population = db.Column(db.String(70))
+    favorites = db.relationship("Favorites", backref="favorites_planet")
+
+    @classmethod
+    def create(cls, body):
+        try:
+            if body.get("planet_name") is None:
+                raise Exception ({
+                    "msg": "Your must write the planet's name",
+                    "status": 400
+                })
+
+            planet_exist = cls.query.filter_by(planet_name = body["planet_name"]).one_or_none()
+
+            if planet_exist:
+                raise Exception ({
+                    "msg": "This planet is already registered. Try another one or adding more info to the name",
+                    "status": 400
+                })
+            
+            new_planet = cls(planet_name = body["planet_name"], climate = body.get("climate"), terrain = body.get("terrain"), population = body.get("population"))
+
+            if not isinstance(new_planet, cls):
+                raise Exception ({
+                    "msg": "Server Error",
+                    "status": 500
+                })
+            
+            save_instance = new_planet.save_and_commit()
+
+            if save_instance is False:
+                raise Exception ({
+                    "msg": "Server Error",
+                    "status": 500
+                })
+
+            return new_planet
+            
+        except Exception as error:
+            return ({
+                "msg": "Something went wrong (" + error.args[0]["msg"] + ")",
+                "status": error.args[0]["status"]
+            })
+
+    def __repr__(self):
+        return f'<Planet {self.planet_name}>'
+
+    def serialize(self):
+        return {
+            "name": self.planet_name
+        }
+
+    def save_and_commit(self):
+        try:
+            db.session.add(self) 
+            db.session.commit()
+            return True
+        except Exception as error:
+            db.session.rollback()
+            return False
+
+class Favorites(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    character_id = db.Column(db.Integer, db.ForeignKey('characters.id'), nullable=True)
+    planet_id = db.Column(db.Integer, db.ForeignKey('planets.id'), nullable=True)
+
+    def fav_char(cls, body, people_id):
+        try:
+            if body.get("user_id") is None:
+                raise Exception ({
+                    "msg": "Your must add the user",
+                    "status": 400
+                })
+
+            selected_fav = Characters.query.get_or_404(people_id)
+
+            fav_exist = cls.query.filter_by(user_id = body["user_id"], character_id = people_id).one_or_none()
+
+            if fav_exist:
+                raise Exception ({
+                    "msg": "This character is already a Favorite. Try another one",
+                    "status": 400
+                })
+
+            new_favorite = cls(user_id= body["user_id"], character_id= people_id)
+         
+            if not isinstance(new_favorite, cls):
+                raise Exception ({
+                    "msg": "Server Error",
+                    "status": 500
+                })
+                
+            save_instance = new_favorite.save_and_commit()
+
+            if save_instance is False:
+                raise Exception ({
+                    "msg": "Server Error",
+                    "status": 500
+                })
+
+            return new_favorite
+
+        except Exception as error:
+            return ({
+                "msg": "Something went wrong (" + error.args[0]["msg"] + ")",
+                "status": error.args[0]["status"]
+            })
+
+    def fav_planet(cls, body, planet_id):
+        try:
+            if body.get("user_id") is None:
+                raise Exception ({
+                    "msg": "Your must add the user",
+                    "status": 400
+                })
+
+            selected_fav = Planets.query.get_or_404(planet_id)
+
+            fav_exist = cls.query.filter_by(user_id = body["user_id"], planet_id = planet_id).one_or_none()
+
+            if fav_exist:
+                raise Exception ({
+                    "msg": "This planet is already a Favorite. Try another one",
+                    "status": 400
+                })
+
+            new_favorite = cls(user_id= body["user_id"], planet_id= planet_id)
+         
+            if not isinstance(new_favorite, cls):
+                raise Exception ({
+                    "msg": "Server Error",
+                    "status": 500
+                })
+                
+            save_instance = new_favorite.save_and_commit()
+
+            if save_instance is False:
+                raise Exception ({
+                    "msg": "Server Error",
+                    "status": 500
+                })
+
+            return new_favorite
+            
+        except Exception as error:
+            return ({
+                "msg": "Something went wrong (" + error.args[0]["msg"] + ")",
+                "status": error.args[0]["status"]
+            })
+
+    def __repr__(self):
+        return f'<Favorites {self.id}>'
+        
+    def serialize(self):
+        return {
+            "id": self.id,
+            "user": self.user_id,
+            "fav_char": self.character_id,
+            "fav_planet": self.planet_id
+        }
+
+    def serialize_char(self):
+        return {
+            "id": self.id,
+            "user": self.user_id,
+            "fav_char": self.character_id
+        }
+    
+    def serialize_planet(self):
+        return {
+            "id": self.id,
+            "user": self.user_id,
+            "fav_planet": self.planet_id
+        }
+    
+    def save_and_commit(self):
+        try:
+            db.session.add(self) 
+            db.session.commit()
+            return True
+        except Exception as error:
+            db.session.rollback()
+            return False
+
+    def delete_and_commit(self):
+        try:
+            db.session.delete(self)
+            db.session.commit()
+            return True
+        except Exception as error:
+            db.session.rollback()
+            return False
